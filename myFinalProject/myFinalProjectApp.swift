942c2b5844fe4e56abeb9cdbe4a5f05014450927
deryosssss <w2115152@westminster.ac.uk>
2025-07-30 14:47:48 +0100

    Initial Commit

---
+++ myFinalProject/myFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFinalProjectApp.swift
+//  myFinalProject
+//
+//  Created by Derya Baglan on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct myFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
